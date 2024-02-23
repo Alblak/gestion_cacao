92ad00b5afa8ab70cdbc50515ab0c42a556be3ea
Alblak <albertkambale16@gmail.com>
2024-02-23 09:11:53 +0200

    add file achat

---
+++ views/achat.php
@@ -0,0 +1,187 @@
+<?php
+include("connexion.php");
+$site=$_SESSION['idsite'];
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">    
+    <title>Margeurita Administration</title>
+    <!-- Vendor CSS Files -->
+   
+    <?php include("link.php"); ?>
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top d-flex align-items-center">
+
+    <?php include('header.php');?>
+
+    </header><!-- End Header -->
+
+    <!-- ======= Sidebar ======= -->
+    <?php include('menu.php');?>
+  <!-- End Sidebar-->
+
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+            <h1></h1>
+            <nav>
+               
+            </nav>
+        </div><!-- End Page Title -->
+        <div>
+           
+        </div>
+        <section class="section" id="entrees" >
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title text-center "> Entrées en Stock</h5>
+                                                    
+                            <!-- Table with stripped rows -->
+                            <?php if(isset($_GET['new'])){ ?>
+                                <form  class="shadow p-3"action="" method="post">
+                                <div class="row">
+                                <div class="col-xl-6 col-lg-6 col-md-6  col-sm-6 p-3">
+                                    <label for="">Choisir l'article</label>
+                                                 <select class="form-select" id="floatingSelect" aria-label="State" name="article" >
+                                                     <?php 
+
+                                                    $repArt= $connexion-> query("SELECT * FROM `article` WHERE statut=0");
+                                                    while ($tab=$repArt->fetch()) {
+                                                        
+                                                    ?>                    
+                                                        <option value="<?php echo $tab['id']; ?>">
+                                                            <?php echo $tab['designation']; ?>
+                                                                                                                        
+                                                        </option>
+                                                    <?php 
+                                                    }
+                                                    ?>                    
+                                                </select>
+                                    </div>
+                                   
+                                    <div class="col-xl-6 col-lg-6 col-md-6  col-sm-6 p-3">
+                                        <label for="">Quantité </span></label>
+                                        <input autocomplete="off" required type="number" class="form-control" placeholder="Entrez la quantite"  name="qte" <?php if(isset($id)){?> value="<?=$ent['quantite']?>"<?php } ?>>
+                                        
+                                        
+                                    </div>
+                                   
+                                    <div class="col-xl-6 col-lg-6 col-md-6  col-sm-6 p-3">
+                                        <label for="">Prix </label>
+                                        <input autocomplete="off" required type="number" class="form-control" placeholder="Entrez le Prix" name="prix"  <?php if(isset($id)){?> value="<?=$ent['prix']?>"<?php } ?>> 
+
+                                    </div>
+                                    <div class="col-xl-6 col-lg-6 col-md-6  col-sm-6 p-3">
+                                    <label for="">Choisir le planteur</label>
+                                         <select class="form-select" id="floatingSelect" aria-label="State" name="fournisseur" >
+                                                        <?php 
+
+                                                        $repChat= $connexion-> query("SELECT * FROM `planteur`");
+                                                        while ($tab=$repChat->fetch()) {
+                                                        
+                                                        ?>                    
+                                                        <option value="<?php echo $tab['idplan']; ?>">
+                                                            <?php echo $tab['nom']; ?>
+                                                            <?php echo $tab['postnom']; ?>                                                            
+                                                            <?php echo $tab['telephone']; ?>                                                            
+                                                            </option>
+                                                        <?php 
+                                                    }
+                                                    ?>                    
+                                          </select>
+                                        </div>
+                      
+                                    </div>
+                                  
+                        <div class="col-xl-12 col-lg-12 col-md-12 mt-10 col-sm-12 p-3 aling-center">
+                           <input type="submit" class="btn btn-dark w-100" name="enregistrer" value="enregistrer">
+                        </div>
+                    </div>
+               
+               
+                                </form>
+
+                            <?php }else{ ?>
+                                <a href="achat.php?new" class=" btn btn-outline-primary bi bi-plus">Nouveau stock</a> 
+
+                            <?php }?>
+                            <table class="table datatable">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">N°</th>
+                                        <th scope="col">Date</th>
+                                        <th scope="col">Article</th>
+                                        <th scope="col">Quantité</th>
+                                        <th scope="col">Fournisseur</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                    <?php 
+                                        $req=$connexion->prepare("SELECT entree.date, article.designation, entree.quantite, planteur.nom, planteur.postnom,entree.id FROM `entree`,article,planteur WHERE article.id=entree.article AND planteur.idplan=entree.planteur AND entree.utilisateur='$site'");
+                                        $req->execute();
+                                        $number=0;
+
+                                        while($donnee=$req->fetch()){
+                                            $number++;
+
+                                        ?>
+
+                                        <tr>
+                                            <th scope="row"><?php echo $number; ?></th>
+                                            <td><?php echo $donnee['0']; ?></td>
+                                            <td><?php echo $donnee['1']; ?></td>
+                                            <td><?php echo $donnee['2']; ?></td>
+                                            <td>
+                                                <?php echo $donnee['3']; ?>
+                                                <?php echo $donnee['4']; ?>
+                                            </td>
+                                            <td><a href="modifarticle.php?iden=<?php echo $donnee['id']?>" class=" btn btn-primary ">Modifier</</td>
+                                        </tr>
+                                        <?php 
+                                        }
+                                    ?>
+                                    
+                                </tbody>
+                            </table>
+                            <!-- End Table with stripped rows -->
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+     
+
+       
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+         <?php include('footer.php'); ?>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <?php include_once('scriptjs.php')?>
+
+</body>
+
+</html>
