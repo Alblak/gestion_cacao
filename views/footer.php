460bde31d4a895e08eca114c1b95e7e6a8e32545
Alblak <albertkambale16@gmail.com>
2024-02-23 09:12:54 +0200

    add footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,7 @@
-<div >
-    &copy; Copyright <strong><span>Margeurita</span></strong>. All Rights Reserved
-</div>
-<div>
-     Designed by <a href="#">devs</a>
+<div class="copyright">
+            &copy; Copyright <strong><span>Margeurita</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+           
+            Designed by <a href="#">Tree Devs</a>
 </div>
